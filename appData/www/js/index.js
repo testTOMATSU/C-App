56d7f7546c4232ec84e61b669c7d103d0c87c179
KatsuyaKubo <katsuya7kubo@gmail.com>
2015-07-03 18:10:25 +0900

    マップ最適化

--- appData/www/js/index.js
+++ appData/www/js/index.js
@@ -166,6 +166,7 @@ var app = {
       console.log("Official page is ready.");
       stopWatch();
     }]);
+    /*
     //公式ページのコントローラ
     module.controller('WebViewController', ['$scope', function($scope) {
       console.log("WebView page is ready.");
@@ -173,7 +174,8 @@ var app = {
       var ref = window.open('http://www.centrair.jp', '_self', 'location=yes');
       ref.addEventListener('loadstart', function() { alert(event.url); });
     }]);
-
+    */
+  
     //キャラ紹介ページのコントローラ
     module.controller('CharacterController', ['$scope', function($scope) {
       console.log("Character page is ready.");
